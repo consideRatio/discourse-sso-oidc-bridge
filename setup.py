561c2b51d147b2dd1b7f561cd757680e6553f43a
Erik Sundell <erik.i.sundell@gmail.com>
2019-02-23 15:16:51 +0100

    Prelim setup.py adjustments

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="discourse-sso-oidc-bridge-consideratio",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Erik Sundell",
+    author_email="erik.i.sundell@gmail.com",
+    description="This flask app can help setup Discourse SSO through OIDC.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/consideratio/discourse-sso-oidc-bridge",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
 )
