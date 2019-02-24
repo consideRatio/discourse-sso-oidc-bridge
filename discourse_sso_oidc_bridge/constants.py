a9b05f053cdee8f18ccec9f43ebad7efec11cfc2
Erik Sundell <erik.i.sundell@gmail.com>
2019-02-24 18:19:59 +0100

    Define known discourse sso attributes

---
+++ discourse_sso_oidc_bridge/constants.py
@@ -0,0 +1,41 @@
+# UPDATED 24 Feb 2019 with values from:
+# https://github.com/discourse/discourse/blob/master/lib/single_sign_on.rb
+
+ALL_ACCESSORS = {
+    "add_groups",
+    "admin",
+    "avatar_force_update",
+    "avatar_url",
+    "bio",
+    "card_background_url",
+    "email",
+    "external_id",
+    "groups",
+    "locale",
+    "locale_force_update",
+    "moderator",
+    "name",
+    "nonce",
+    "profile_background_url",
+    "remove_groups",
+    "require_activation",
+    "return_sso_url",
+    "suppress_welcome_message",
+    "title",
+    "username",
+    "website",
+}
+
+BOOL_ACCESSORS = {
+    "admin",
+    "avatar_force_update",
+    "locale_force_update",
+    "moderator",
+    "require_activation",
+    "suppress_welcome_message",
+}
+
+REQUIRED_ACCESSORS = {
+    "email",
+    "external_id",
+}
