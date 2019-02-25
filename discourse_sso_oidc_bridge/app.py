ff6ee50deb3124f8b144d6af9d05c59614fda789
Erik Sundell <erik.i.sundell@gmail.com>
2019-02-25 16:55:39 +0100

    Started using create_app() for Dockerization

---
+++ discourse_sso_oidc_bridge/app.py
@@ -0,0 +1,227 @@
+"""
+SSO FLASK Application for Discourse
+The configuration file is defined with the variable "DISCOURSE_SSO_CONFIG",
+for the most significant values look at the sso/default.py file
+"""
+
+from flask import abort, Flask, redirect, render_template, request, url_for, session
+from flask_pyoidc.flask_pyoidc import OIDCAuthentication
+from flask_pyoidc.provider_configuration import ProviderConfiguration, ProviderMetadata, ClientMetadata
+
+import os
+import base64
+import hashlib
+import hmac
+import requests
+import json
+from urllib.parse import quote
+from .constants import ALL_ATTRIBUTES, BOOL_ATTRIBUTES, REQUIRED_ATTRIBUTES
+from .default import Config as DefaultConfig
+
+# Disable SSL certificate verification warning
+requests.packages.urllib3.disable_warnings()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load application config from various sources
+    # ------------------------------------------------------------------------------
+    # 1. Defaults from this package
+    app.config.from_object(DefaultConfig)
+
+    # 2. From a config.py file in the application directory
+    app.config.from_pyfile(
+        filename=os.path.join(os.getcwd(), "config.py"),
+        silent=True
+    )
+
+    # 3. From a dynamically configurable file location
+    if os.environ.get('CONFIG_LOCATION'):
+        app.config.from_pyfile(
+            filename=os.environ.get('CONFIG_LOCATION'),
+            silent=False
+        )
+
+
+    # Initialize OpenID Connect extension
+    # ------------------------------------------------------------------------------
+
+    # If explicit OIDC provider information is provided, we use that instead of the
+    # information dynamically provided by the .well-known/openid-configuration
+    # endpoint.
+
+    # https://github.com/zamzterz/Flask-pyoidc#dynamic-provider-configuration
+    client_metadata = ClientMetadata(**app.config['OIDC_CLIENT_METADATA'])
+
+    if app.config['OIDC_PROVIDER_METADATA']:
+        provider_metadata = ProviderMetadata(**app.config['OIDC_PROVIDER_METADATA'])
+        provider = ProviderConfiguration(
+            provider_metadata=provider_metadata,
+            client_metadata=client_metadata,
+            auth_request_params=app.config['OIDC_AUTH_REQUEST_PARAMS'],
+        )
+    else:
+        provider = ProviderConfiguration(
+            issuer=app.config['OIDC_ISSUER'],
+            client_metadata=client_metadata,
+            auth_request_params=app.config['OIDC_AUTH_REQUEST_PARAMS'],
+        )
+
+    auth = OIDCAuthentication(
+        provider_configurations={
+            'default': provider,
+        },
+        app=app,
+    )
+
+
+    @app.route('/')
+    def index():
+        """
+        If a user tries to access this application directly,
+        just redirect them to Discourse.
+        :return: Redirect to the configurated DISCOURSE_URL
+        """
+        return redirect(app.config.get('DISCOURSE_URL'), 302)
+
+
+    @app.route('/sso/login')
+    def payload_check():
+        """
+        Verify the payload and signature coming from a Discourse server and if
+        correct redirect to the authentication page.
+        :return: The redirection page to the authentication page
+        """
+
+        # Get payload and signature from Discourse request
+        payload = request.args.get('sso', '')
+        signature = request.args.get('sig', '')
+
+        if not payload or not signature:
+            abort(400)
+
+        app.logger.debug('Request to login with payload="%s" signature="%s"', payload, signature)
+        app.logger.debug('Session Secret Key: %s', app.secret_key)
+        app.logger.debug('SSO Secret Key: %s', app.config.get('DISCOURSE_SECRET_KEY'))
+
+        # Calculate and compare request signature
+        dig = hmac.new(app.config.get('DISCOURSE_SECRET_KEY', '').encode('utf-8'),
+                    payload.encode('utf-8'),
+                    hashlib.sha256).hexdigest()
+        app.logger.debug('Calculated hash: %s', dig)
+
+        if dig != signature:
+            abort(400)
+
+        # Decode the payload and store in session
+        decoded_msg = base64.b64decode(payload).decode('utf-8')
+        session['discourse_nonce'] = decoded_msg  # This can't just be 'nonce' as Flask-pyoidc will steamroll it
+
+        # Redirect to authorization endpoint
+        return redirect(url_for('user_authz'))
+
+
+    @app.route('/sso/auth')
+    @auth.oidc_auth('default')
+    def user_authz():
+        """
+        Read the user attributes provided by Flask-pyoidc and
+        create the payload to send to Discourse.
+        :return: The redirection page to Discourse
+        """
+
+        # Check to make sure we have a valid session
+        if 'discourse_nonce' not in session:
+            app.logger.debug('Discourse nonce not found in session')
+            abort(403)
+
+        attribute_map = app.config.get('USERINFO_SSO_MAP')
+
+        sso_attributes = {}
+        userinfo = session['userinfo']
+
+        # Check if the provided userinfo should be used to set information to be
+        # passed to discourse. Do it by checking if the userinfo field is...
+        # 1. explicitly mapped using the provided map 
+        # 2. if it can match one of the known attributes with discourse_ prefixed
+        # 3. if it can match one of the known attributes directly
+        for userinfo_key, userinfo_value in userinfo.items():
+            attribute_key = attribute_map.get(userinfo_key)
+
+            if attribute_key:
+                pass
+            elif ("discourse_" + userinfo_key) in ALL_ATTRIBUTES:
+                attribute_key = "discourse_" + userinfo_key
+            elif userinfo_key in ALL_ATTRIBUTES:
+                attribute_key = userinfo_key
+
+            if attribute_key:
+                if attribute_key in BOOL_ATTRIBUTES:
+                    userinfo_value = "false" if str.lower(str(userinfo_value)) in ['false', 'f', '0'] else "true"
+                sso_attributes[attribute_key] = userinfo_value
+        
+        # Check if we have a default value that should be utilized
+        default_sso_attributes = app.config.get('DEFAULT_SSO_ATTRIBUTES')
+        for default_attribute_key, default_attribute_value in default_sso_attributes.items():
+            if not default_attribute_key in sso_attributes:
+                sso_attributes[default_attribute_key] = default_attribute_value
+
+        # Check if we got the required attributes
+        for required_attribute in REQUIRED_ATTRIBUTES:
+            if not sso_attributes.get(required_attribute):
+                app.logger.debug(f'{required_attribute} not found in userinfo: ' + json.dumps(session['userinfo']))
+                abort(403)
+
+        # All systems are go!
+        app.logger.debug(f'Authenticating "{sso_attributes.get("external_id")}", named "{sso_attributes.get("name")}" with email: "{sso_attributes.get("email")}"')
+
+        # Construct the response inner query parameters
+        query = session['discourse_nonce']
+        for sso_attribute_key, sso_attribute_value in sso_attributes.items():
+            query += f'&{sso_attribute_key}={quote(sso_attribute_value)}'
+        app.logger.debug('Query string to return: %s', query)
+
+        # Encode response
+        query_b64 = base64.b64encode(query.encode('utf-8'))
+        app.logger.debug('Base64 query string to return: %s', query_b64)
+
+        # Build URL-safe response
+        query_urlenc = quote(query_b64)
+        app.logger.debug('URLEnc query string to return: %s', query_urlenc)
+
+        # Generate signature for response
+        sig = hmac.new(app.config.get('DISCOURSE_SECRET_KEY').encode('utf-8'),
+                    query_b64,
+                    hashlib.sha256).hexdigest()
+        app.logger.debug('Signature: %s', sig)
+
+        # Build redirect URL
+        redirect_url = (app.config.get('DISCOURSE_URL') +
+                        '/session/sso_login?'
+                        'sso=' + query_urlenc +
+                        '&sig=' + sig)
+
+        # Redirect back to Discourse
+        return redirect(redirect_url)
+
+
+    @app.route('/logout')
+    @auth.oidc_logout
+    def logout():
+        """
+        Handle logging a user out. Flask-pyoidc does the heavy lifting here.
+        :return: Redirect to the application index
+        """
+        return redirect(url_for('index'), 302)
+
+
+    @app.errorhandler(403)
+    def attribute_not_provided(error):
+        """
+        Render a custom error page in case the IdP authenticate the user but does
+        not provide the requested attributes
+        :type error: object
+        """
+        return render_template('403.html'), 403
+
+    return app
---
+++ tests/test_app.py
@@ -0,0 +1,132 @@
+# Copyright 2015 INFN
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+SSO Application tests
+"""
+
+import time
+from flask import url_for
+import pytest
+from urllib.parse import urlparse
+from discourse_sso_oidc_bridge.app import create_app
+
+@pytest.fixture
+def client():
+    app = create_app()
+
+    # Flask provides a way to test your application by exposing the Werkzeug test Client
+    # and handling the context locals for you.
+    client = app.test_client()
+
+    # Establish an application context before running the tests.
+    ctx = app.app_context()
+    ctx.push()
+    yield client  # this is where the testing happens!
+    ctx.pop()
+
+
+@pytest.fixture
+def auth_data():
+    return {
+        'access_token': 'test_access_token',
+        'id_token': {
+            'iss': 'issuer1',
+            'aud': 'client1',
+            'sub': 'john_doe',
+            'name': 'John Doe',
+            'iat': 1550961213,
+            'exp': 1550964813,
+        },
+        # INSPECT id_token_jwt at:
+        # https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJpc3N1ZXIxIiwiYXVkIjoiY2xpZW50MSIsInN1YiI6ImpvaG5fZG9lIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTUwOTYxMjEzLCJleHAiOjE1NTA5NjQ4MTN9.eiBu_D5m4vPVd11C-phlKkHha2VPlmaRQPTuV49QTIk
+        'id_token_jwt': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+                        '.eyJpc3MiOiJpc3N1ZXIxIiwiYXVkIjoiY2xpZW50MSIsInN1YiI6ImpvaG5fZG9lIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTUwOTYxMjEzLCJleHAiOjE1NTA5NjQ4MTN9'
+                        '.eiBu_D5m4vPVd11C-phlKkHha2VPlmaRQPTuV49QTIk',
+        'userinfo': {
+            'sub': 'john_doe',
+            'name': 'John Doe',
+            'email': 'john_doe@example.com',
+            'preferred_username': 'john_doe',
+        },
+        'last_authenticated': time.time(),
+    }
+
+
+@pytest.fixture
+def discourse_nonce():
+    return {
+        'discourse_nonce': 'nonce=cb68251eefb5211e58c00ff1395f0c0b',
+    }
+
+
+def test_payload_check(client):
+    """Test the payload is properly managed and the user is sent to the
+    authentication page
+    """
+    with client.get(
+        '/sso/login?sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI%3D%0A&'
+        'sig=d87265a513d3fa4c7602ada38cfa60318c9804da6c95785ab36885dc79641671'
+    ) as res:
+        assert res.status_code == 302
+        assert urlparse(res.location).path == '/sso/auth'
+
+
+def test_bad_payload_sig(client):
+    """Test the error code 400 is sent if the signature do not match
+    the payload
+    """
+    with client.get(
+        '/sso/login?sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI%3D%0A&'
+        'sig=2828aa29899722b35a2f191d34ef9b3ce695e0e6eeec47deb46d588d70c7cb58'
+    ) as res:
+        assert res.status_code == 400
+
+
+def test_no_payload(client):
+    """Test the error code 400 is sent if the sso field is not provided"""
+    with client.get(
+        '/sso/login?sig=2828aa29899722b35a2f191d34ef9b3ce695e0e6eeec47deb46d588d70c7cb56'
+    ) as res:
+        assert res.status_code == 400
+
+
+def test_no_hash(client):
+    """Test the error code 400 is sent if the sig field is not provided"""
+    with client.get(
+        '/sso/login?sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGI%3D%0A&'
+    ) as res:
+        assert res.status_code == 400
+
+
+def test_authentication_state_not_available(client, discourse_nonce):
+    """Test the authentication are properly send to Discourse"""
+    with client.session_transaction() as session:
+        session.update(discourse_nonce)
+
+    res = client.get('/sso/auth')
+    assert res.status_code == 302
+    assert '?client_id=dummy_client_id&response_type=code&scope=openid+profile&redirect_uri=http%3A%2F%2Fdiscourse-sso.example.com%2Fredirect_uri' in res.location
+
+
+def test_authentication_state_available(client, discourse_nonce, auth_data):
+    """Test the authentication are properly send to Discourse"""
+    with client.session_transaction() as session:
+        session.update(discourse_nonce)
+        session.update(auth_data)
+
+    res = client.get('/sso/auth')
+    assert res.status_code == 302
+    assert res.location == 'http://discourse.example.com/session/sso_login?sso=bm9uY2U9Y2I2ODI1MWVlZmI1MjExZTU4YzAwZmYxMzk1ZjBjMGImZW1haWw9am9obl9kb2UlNDBleGFtcGxlLmNvbSZuYW1lPUpvaG4lMjBEb2UmdXNlcm5hbWU9am9obl9kb2UmZXh0ZXJuYWxfaWQ9am9obl9kb2U%3D&sig=4e4617a08d20f8380e10ba801879e79700cd14051da6341fc93c8563d50a155e'
